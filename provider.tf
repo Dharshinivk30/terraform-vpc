4ac4a47df94ac168cdc85c0d1fe221f0587a0fa1
dharshini <dharshinivk30@gmail.com>
2024-02-23 11:55:02 +0530

    first

---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Provider Block
+provider "aws" {
+  region  = var.aws_region
+}
